6529d5eceea394e244a7ee610ef531d3945f4b45
danieltorres4 <dst990502@outlook.com>
2023-03-02 10:10:25 -0600

    Initial Commit

---
+++ ColorPalette/ColorPaletteApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorPaletteApp.swift
+//  ColorPalette
+//
+//  Created by Iván Sánchez Torres on 02/03/23.
+//
+
+import SwiftUI
+
+@main
+struct ColorPaletteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
